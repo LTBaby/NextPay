57c8f806943201f9c74a93436a4c42e2d8f590aa
LT69Baby <trevor998tredoux@gmail.com>
2022-10-20 01:12:10 +0200

    okay

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// 7 Type of Graph using React Native Chart Kit
+// https://aboutreact.com/react-native-chart-kit/
 
-export default function App() {
+// import React in our code
+import React from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+
+//import React Native chart Kit for different kind of Chart
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+
+const MyProgressChart = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text style={styles.header}>Progress Chart</Text>
+      <ProgressChart
+        data={[0.4]}
+        width={Dimensions.get('window').width }
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView>
+        <Text style={styles.lab}>Next Payday: </Text>
+        <View style={styles.container}>
+          <View>
+            <MyProgressChart />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
   },
+  header: {
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 16,
+    marginTop: 16,
+  },
+  lab: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 16,
+    marginTop: 16,
+    fontSize: 22,
+  }
 });
